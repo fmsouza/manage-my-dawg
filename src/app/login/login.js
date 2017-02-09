d1d7e2bfe6ec6e34f2b832deb828c7aab82586df
Fred Souza <fredericoamsouza@gmail.com>
2017-02-09 18:31:02 -0200

    Creating login form

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Button, FormControl, FormGroup, InputGroup } from 'react-bootstrap';
+import './login.css';
 
 export default class Login extends React.Component {
 
     render() {
         return (
-            <div></div>
+            <div className="login">
+                <h1 className="center">Login</h1>
+                <form>
+                    <FormGroup controlId="fieldUsername">
+                        <InputGroup>
+                            <InputGroup.Addon>Username</InputGroup.Addon>
+                            <FormControl type="text" placeholder="doglord"/>
+                        </InputGroup>
+                    </FormGroup>
+                    <FormGroup controlId="fieldPassword">
+                        <InputGroup>
+                            <InputGroup.Addon>Password</InputGroup.Addon>
+                            <FormControl type="password" placeholder="********"/>
+                        </InputGroup>
+                    </FormGroup>
+
+                    <FormGroup className="center">
+                        <Button type="submit">
+                            Sign in
+                        </Button>
+                    </FormGroup>
+                </form>
+            </div>
         );
     }
 }
