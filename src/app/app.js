56392a3d9436753056b07b1f9535695d2ce4c775
Fred Souza <fredericoamsouza@gmail.com>
2017-02-09 17:59:32 -0200

    Refactoring app component and making it functional

--- src/app/app.js
+++ src/app/app.js
@@ -1,57 +1,11 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import Header from './header';
 
-export default class App extends React.Component {
-  state = { username: '', token: '' };
-
-  componentWillMount() {
-    const { username, token } = this.state;
-    // if (!username || !token) window.location = "#/login";
-  }
-
-  onClickLogout() {
-    console.log("Clicked in logout button");
-  }
-
-  getUserMenu() {
-    return (
-      <Nav pullRight>
-        <NavItem eventKey={1} href={`/dogs/${this.state.username}`}>My Dogs</NavItem>
-        <NavItem eventKey={2} href="/dogs">View all dogs</NavItem>
-        <NavItem eventKey={3} href="/users">View all users</NavItem>
-        <NavItem eventKey={4} href="/logout">Log out</NavItem>
-      </Nav>
-    );
-  }
-
-  getLoginMenu() {
-    return (
-      <Nav pullRight>
-        <NavItem eventKey={4} href="/login">Login</NavItem>
-      </Nav>
-    );
-  }
-
-  getMenu() {
-    const { username, token } = this.state;
-    return (!username || !token) ? this.getLoginMenu() : this.getUserMenu();
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="/">Manage my dawg</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          {this.getMenu()}
-        </Navbar>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+export default props => (
+  <div>
+    <Header />
+    {props.children}
+  </div>
+);
