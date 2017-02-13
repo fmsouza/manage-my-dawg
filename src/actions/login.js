a18f868dc17d78d0b34775cc0c4701bd3514249b
Fred Souza <fredericoamsouza@gmail.com>
2017-02-13 16:25:29 -0200

    Fixing login action dispatcher

--- src/actions/login.js
+++ src/actions/login.js
@@ -10,7 +10,7 @@ export const LOGIN_CODE_SUCCESS = 200;
 export function attemptLogin(user, pass) {
     return dispatch => {
         axios.post(`http://devnetwork.watt-now.nl:8888/authenticate`, { user, pass })
-            .catch(error => dispatch({ type: LOGIN_RESULT_FAILED }))
-            .then(data => dispatch({ type: LOGIN_RESULT_SUCCESS, payload: data }));
+            .catch(error => { dispatch({ type: LOGIN_RESULT_FAILED }) })
+            .then(data => { if (data) dispatch({ type: LOGIN_RESULT_SUCCESS, payload: data }) });
     };
 }
